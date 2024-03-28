f753520f0131953fd7d125d27fa455f4908fd984
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-03-28 08:48:05 +0530

    login logout page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import Register from "./pages/Register";
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="w-full h-screen bg-green-300">
+      <Routes>
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
-
 export default App;
